8e36fee672210dc0a7377b6a8f7d10f9f4d04fb7
Tarasa24 <petr.gajdosik2424@gmail.com>
2019-11-23 16:08:43 +0100

    cities loading optimisation

--- python/closest_city.py
+++ python/closest_city.py
@@ -3,21 +3,25 @@ from scipy import spatial
 import json
 
 
+cities_ets2 = None
+with open('dbs/cities/cities_ets2.json', encoding='utf-8') as x:
+    cities_ets2 = json.load(x)
+
+cities_ats = None
+with open('dbs/cities/cities_ats.json', encoding='utf-8') as y:
+    cities_ats = json.load(y)
+
 def chooseJson():
     if processes.is_running("eurotrucks2.exe"):
-        with open('dbs/cities/cities_ets2.json', encoding='utf-8') as x:
-            data = json.load(x)
-            cities = []
-            for e in data["citiesList"]:
-                cities.append((float(e["x"]), float(e["z"])))
-            return (cities, data)
+        cities = []
+        for e in cities_ets2["citiesList"]:
+            cities.append((float(e["x"]), float(e["z"])))
+        return (cities, cities_ets2)
     elif processes.is_running("amtrucks.exe"):
-        with open('dbs/cities/cities_ats.json', encoding='utf-8') as y:
-            data = json.load(y)
-            cities = []
-            for e in data["citiesList"]:
-                cities.append((float(e["x"]), float(e["z"])))
-            return (cities, data)
+        cities = []
+        for e in cities_ats["citiesList"]:
+            cities.append((float(e["x"]), float(e["z"])))
+        return (cities, cities_ats)
 
 
 def ccQuerry(coord):
