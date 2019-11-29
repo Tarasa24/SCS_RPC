a83a53aa8e8a2d5001bf211c91bf70f4807a73a3
Tarasa24 <petr.gajdosik2424@gmail.com>
2019-11-29 21:53:06 +0100

    building

---
+++ build/setup.py
@@ -0,0 +1,29 @@
+import sys
+from cx_Freeze import setup, Executable
+import os
+
+
+# Dependencies are automatically detected, but it might need fine tuning.
+build_exe_options = {"packages": ["psutil", "scipy.spatial", "scipy._distributor_init", "scipy.sparse.csgraph._validation"],
+                     "excludes": ["scipy.spatial.cKDTree"],
+                     "include_files": ["../src/modules", "../src/dbs", "../src/config.json"],
+                     "build_exe": "out"}
+
+app = Executable(
+    script="../src/app.py",
+    base=None,
+    icon="scs_ico.ico",
+    targetName="SCS_RPC.exe"
+    )
+
+status = Executable(
+    script="../src/status.py",
+    base=None,
+    targetName="status.exe"
+    )
+
+setup(name = "SCS_RPC",
+      version = "0.1",
+      description = "Unofficial Open-source Discord Rich Presence for ETS2 and ATS",
+      options = {"build_exe": build_exe_options},
+      executables = [app, status])
